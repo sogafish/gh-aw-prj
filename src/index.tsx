f8a861c747b693d07183779558ae8d28430d26e3
sogafish <kent.soga@gmail.com>
2026-02-17 21:17:35 +0900

    :sparkles: Add New endpoint

--- src/index.tsx
+++ src/index.tsx
@@ -9,4 +9,10 @@ app.get("/", (c) => {
   return c.render(<h1>Hello!</h1>);
 });
 
+app.get("/echo/:text", (c) => {
+  const text = c.req.param("text");
+
+  return c.render(<h1>{text}!!</h1>);
+});
+
 export default app;

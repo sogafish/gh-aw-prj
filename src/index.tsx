01e11fc2525b96218ea5a26fb6e64d61e52ffd0c
sogafish <kent.soga@gmail.com>
2026-02-17 21:55:33 +0900

    :sparkles: Add Endpoint

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import { Hono } from "hono";
 import { renderer } from "./renderer";
+import { getPrimeNumbers } from "./utils";
 
 const app = new Hono();
 
@@ -13,6 +14,21 @@ app.get("/echo/:text", (c) => {
   const text = c.req.param("text");
 
   return c.render(<h1>{text}!!</h1>);
+});
+
+app.get("/num", (c) => {
+  const numbers = getPrimeNumbers(10);
+
+  return c.render(
+    <div>
+      <h1>10この素数</h1>
+      <ul>
+        {numbers.map((num) => (
+          <li key={num}>{num}</li>
+        ))}
+      </ul>
+    </div>,
+  );
 });
 
 export default app;
